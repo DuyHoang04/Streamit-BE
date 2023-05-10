77532405a1dd5c6089d62bc61acd63c3dbeeac25
DuyHoang <duyhoang220904@gmail.com>
2023-05-10 17:17:27 +0700

    crud genres

--- src/models/genresModel.js
+++ src/models/genresModel.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
-const genresSchema = new mongoose.Schema({
-  name: String,
-  description: {
-    type: String,
-  },
-  movies: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Movie",
+const genresSchema = new mongoose.Schema(
+  {
+    name: String,
+    description: {
+      type: String,
     },
-  ],
-});
+    movies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const genresModel = mongoose.model("Genres", genresSchema);
 
